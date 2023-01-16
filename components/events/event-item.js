0a1b60683da910ecb3c55b190168811020cbbf62
dongmin7208 <sayg3o3d@gmail.com>
2023-01-16 15:20:11 +0900

    ADD: Image Optimizer

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import classes from './event-item.module.css';
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
@@ -16,7 +17,7 @@ export default function EventItem(props) {
   return (
     <li className={classes.item}>
       {/* <picture> */}
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       {/* </picture> */}
       <div className={classes.content}>
         <div className={classes.summary}>
